98fb100a33be7e3f75bbf230409c78b636814316
Ryan1729 <Ryan1729@gmail.com>
2021-04-26 01:06:00 -0600

    In an excessive display of bit-twiddling, write const usable f32 comparisons
    Still not usable in const fn though

--- libs/f32_is/src/f32_is.rs
+++ libs/f32_is/src/f32_is.rs
@@ -39,6 +39,94 @@ macro_rules! neg_normal_and_above_negative_one {
     }}
 }
 
+#[macro_export]
+macro_rules! lt {
+    ($f32_a: expr, $f32_b: expr) => {{
+        // This is a const version of `<`
+        let a_bits = unsafe {
+            core::mem::transmute::<f32, i32>($f32_a)
+        };
+
+        let b_bits = unsafe {
+            core::mem::transmute::<f32, i32>($f32_b)
+        };
+
+        // Without this transformation sorting [1., 2., -1., -2.] in ascending order
+        // with this operator will result in [ -1., -2., 1., 2.,] instead of the
+        // expected [-2., -1., 1., 2.,].
+        let a_two_comp = a_bits ^ ((a_bits >> 31) & 0x7fff_ffff);
+        let b_two_comp = b_bits ^ ((b_bits >> 31) & 0x7fff_ffff);
+
+        a_two_comp < b_two_comp
+    }}
+}
+
+#[macro_export]
+macro_rules! le {
+    ($f32_a: expr, $f32_b: expr) => {{
+        // This is a const version of `<=`
+        let a_bits = unsafe {
+            core::mem::transmute::<f32, i32>($f32_a)
+        };
+
+        let b_bits = unsafe {
+            core::mem::transmute::<f32, i32>($f32_b)
+        };
+
+        // Without this transformation sorting [1., 2., -1., -2.] in ascending order
+        // with this operator will result in [ -1., -2., 1., 2.,] instead of the
+        // expected [-2., -1., 1., 2.,].
+        let a_two_comp = a_bits ^ ((a_bits >> 31) & 0x7fff_ffff);
+        let b_two_comp = b_bits ^ ((b_bits >> 31) & 0x7fff_ffff);
+
+        a_two_comp <= b_two_comp
+    }}
+}
+
+#[macro_export]
+macro_rules! gt {
+    ($f32_a: expr, $f32_b: expr) => {{
+        // This is a const version of `>`
+        let a_bits = unsafe {
+            core::mem::transmute::<f32, i32>($f32_a)
+        };
+
+        let b_bits = unsafe {
+            core::mem::transmute::<f32, i32>($f32_b)
+        };
+
+        // Without this transformation sorting [1., 2., -1., -2.] in ascending order
+        // with this operator will result in [-1., -2., 1., 2.,] instead of the
+        // expected [-2., -1., 1., 2.,].
+        let a_two_comp = a_bits ^ ((a_bits >> 31) & 0x7fff_ffff);
+        let b_two_comp = b_bits ^ ((b_bits >> 31) & 0x7fff_ffff);
+
+        a_two_comp > b_two_comp
+    }}
+}
+
+#[macro_export]
+macro_rules! ge {
+    ($f32_a: expr, $f32_b: expr) => {{
+        // This is a const version of `>=`
+        let a_bits = unsafe {
+            core::mem::transmute::<f32, i32>($f32_a)
+        };
+
+        let b_bits = unsafe {
+            core::mem::transmute::<f32, i32>($f32_b)
+        };
+
+        // Without this transformation sorting [1., 2., -1., -2.] in ascending order
+        // with this operator will result in [-1., -2., 1., 2.] instead of the
+        // expected [-2., -1., 1., 2.].
+        let a_two_comp = a_bits ^ ((a_bits >> 31) & 0x7fff_ffff);
+        let b_two_comp = b_bits ^ ((b_bits >> 31) & 0x7fff_ffff);
+
+        a_two_comp >= b_two_comp
+    }}
+}
+
 #[cfg(test)]
 mod tests {
     macro_rules! use_the_macros {
@@ -47,12 +135,17 @@ mod tests {
             && !pos_zero!(-0.0)
             && pos_normal_and_one_or_below!(0.5)
             && neg_normal_and_above_negative_one!(-0.5)
+            && lt!(-0.5,  0.5)
+            && le!(-0.5, -0.5)
+            && gt!( 0.5, -0.5)
+            && ge!( 0.5,  0.5)
         }}
     }
 
     #[test]
     fn the_macros_are_usable_in_consts() {
-        assert!(use_the_macros!())
+        const A: bool = use_the_macros!();
+        assert!(A)
     }
 
     /* Floating point operations are currently not permitted in const fn on stable 
@@ -66,4 +159,72 @@ mod tests {
         assert!(i_am_const_fn());
     }
     */
+
+    #[test]
+    fn sorting_with_lt_works_as_claimed() {
+        use core::cmp::Ordering::*;
+        let unsorted = [1., 2., -1., -2.];
+        let expected = [-2., -1., 1., 2.];
+
+        let mut sorted = unsorted;
+        // We rely on the list having no equal elements.
+        sorted.sort_by(|a, b| if lt!(*a, *b) {
+            Less
+        } else {
+            Greater
+        });
+
+        assert_eq!(sorted, expected);
+    }
+
+    #[test]
+    fn sorting_with_le_works_as_claimed() {
+        use core::cmp::Ordering::*;
+        let unsorted = [1., 2., -1., -2.];
+        let expected = [-2., -1., 1., 2.];
+
+        let mut sorted = unsorted;
+        // We rely on the list having no equal elements.
+        sorted.sort_by(|a, b| if le!(*a, *b) {
+            Less
+        } else {
+            Greater
+        });
+
+        assert_eq!(sorted, expected);
+    }
+
+    #[test]
+    fn sorting_with_gt_works_as_claimed() {
+        use core::cmp::Ordering::*;
+        let unsorted = [1., 2., -1., -2.];
+        let expected = [-2., -1., 1., 2.];
+
+        let mut sorted = unsorted;
+        // We rely on the list having no equal elements.
+        sorted.sort_by(|a, b| if gt!(*a, *b) {
+            Greater
+        } else {
+            Less
+        });
+
+        assert_eq!(sorted, expected);
+    }
+
+    #[test]
+    fn sorting_with_ge_works_as_claimed() {
+        use core::cmp::Ordering::*;
+        let unsorted = [1., 2., -1., -2.];
+        let expected = [-2., -1., 1., 2.];
+
+        let mut sorted = unsorted;
+        // We rely on the list having no equal elements.
+        sorted.sort_by(|a, b| if ge!(*a, *b) {
+            Greater
+        } else {
+            Less
+        });
+
+        assert_eq!(sorted, expected);
+    }
 }
